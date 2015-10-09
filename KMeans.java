e46f17df9b2834cb5bcb99a98d7dcad846532614
Remco32 <remco32@gmail.com>
2015-10-09 16:30:18 +0200

    Finished step 2 of training in Kmeans.

--- KMeans.java
+++ KMeans.java
@@ -83,29 +83,28 @@ public class KMeans extends ClusteringAlgorithm
 
 
 		// Step 2: Generate a new partition by assigning each datapoint to its closest cluster center
-		///Go through all the points. i is current person.
+		///Go through all the people. i is current person.
 		for(int i = 0; i < 70; i++){ ///TODO change to variable
 			int closestCenter = -1;
 			float shortestDistance = 9999;
-			float newDistance = 9999;
+			float newDistance = 0;
 
 			///Calculate distances to each initial point of each cluster. There are k clusters. j is current cluster.
 			for(int j = 0; j < k; j++){
 				///Calculate distance ///TODO Add Euclidian distance using formula from the assignment.
-				for (int h = 0; h < dim; h++){ ///Vector is 200 wide  ///TODO debug this
-					///sum it
-					newDistance = trainData.get(j)[h] - clusters[j].prototype[h];
-					///square it
-					newDistance = newDistance*newDistance;
+				for (int h = 0; h < dim; h++){  ///TODO debug this
+					newDistance = newDistance + (float)Math.pow((trainData.get(i)[h] - clusters[j].prototype[h]),2);
+					///System.out.println("newDistance = "+ newDistance+" at website "+h);
 				}
-				///get the square root
-				newDistance = (float)Math.sqrt(newDistance);
-				System.out.println("newDistance is berekend op "+ newDistance);
+
+				System.out.println("newDistance is berekend op "+ newDistance+" voor cluster "+j);
 
 				if (newDistance <= shortestDistance){
 					closestCenter = j;
+					shortestDistance = newDistance;
 					System.out.println("Cluster "+closestCenter+" is now the closest with distance "+shortestDistance);
 				}
+				newDistance = 0; /// clean the variable for the next cluster
 			}
 			///In case something went wrong.
 			if (closestCenter == -1){
