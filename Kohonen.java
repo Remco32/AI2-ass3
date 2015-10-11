0a52cf4d04926c054eb23ba3ca2a092ae7757355
¨wolkia¨ <claudiafrank75@gmail.com>
2015-10-12 00:43:14 +0200

    ¨test¨

--- Kohonen.java
+++ Kohonen.java
@@ -33,10 +33,12 @@ public class Kohonen extends ClusteringAlgorithm
 			float[] prototype;
 
 			Set<Integer> currentMembers;
+			Set<Integer> previousMembers;
 
 			public Cluster()
 			{
 				currentMembers = new HashSet<Integer>();
+				previousMembers = new HashSet<Integer>();
 			}
 	}
 	
@@ -54,63 +56,102 @@ public class Kohonen extends ClusteringAlgorithm
 		
 
 
-		// Here n*n new cluster are initialized and the cluster centers are initialized as random;
+		// Here n*n new cluster are initialized
 		clusters = new Cluster[n][n];
 		for (int i = 0; i < n; i++)  {
 			for (int i2 = 0; i2 < n; i2++) {
 				clusters[i][i2] = new Cluster();
 				clusters[i][i2].prototype = new float[dim];
-				int R = rnd.nextInt(dim-0);
+				int R = rnd.nextInt(trainData.size());
+				
+				//cluster centers are initialized as random
 				clusters[i][i2].currentMembers.add(R);
+				clusters[i][i2].prototype = calculatePrototype(clusters[i][i2]);
 			}
 		}
 	}
 
-	public Integer calcBMU(Object element){
-		Integer BMU;
-		
-		//go through all clustercenter and calculate the euclidean distance and the closest cluster center as BMU 
-		
-		//return BMU;
-		return 0; /// Set to 0 so the code can compile
-	}
 	
 	public boolean train()
 	{
- 
+        
 		for(int k =0; k<epochs; k++){
 			//calculate new neighborehoodsize and learning rate
-		    float rad = (n/2) * ( 1 - (k/epochs));
+		    int rad = (n/2) * ( 1 - (k/epochs));
 		    double learnr= initialLearningRate * (1 - (k/epochs));
 		    
-		    //interate through traindata vectors
-			Iterator it = trainData.iterator();
-            while(it.hasNext()){
-            	Object element = it.next();
-            	Integer BMU = calcBMU(element); //where the fuck do i save the BMU
-            	//changes the current members within that BMU to the new formula 
-            	//go through clusters 
-            	for (int i = 0; i < n; i++)  {
-        			for (int i2 = 0; i2 < n; i2++) {
-        				//find if value is in neighborehood 
-        				for (Integer value : clusters[i][i2].currentMembers) {
-        					//if value is between BMU +- rad 
-        					//if () {
-        				    	//change that value according to the formula pnew = (1-learnr) * pold + learr * X;
-        				      // value = (1- learnr)* value + learnr * element; 
-        				        
-        				   // }
-        				}
-        			}
-        		}			    
+            	// Step 2: get the BMU which is the closest cluster center of the datapoint and adjust its prototype to the formula. 
+    			///Go through all the people. i is current person.
+    			for (int i = 0; i < trainData.size(); i++) {
+    				int closestCenter = -1;
+    				int closestCenter2 = -1;
+    				float shortestDistance = 9999;
+    				float newDistance = 0;
 
-            }
-		
+    				///Calculate distances to each initial point of each cluster. There are n*n clusters. j is current cluster.
+    				for (int j = 0; j < n; j++) {
+    					for (int j2 = 0; j2 < n; j2++) {
+    					///Calculate distance
+    					for (int h = 0; h < dim; h++) {
+    						newDistance = newDistance + (float) Math.pow((trainData.get(i)[h] - clusters[j][j2].prototype[h]), 2);
+    						///System.out.println("newDistance = "+ newDistance+" at website "+h);
+    					}
+
+    					///System.out.println("newDistance is berekend op "+ newDistance+" voor cluster "+j);
+
+    					if (newDistance <= shortestDistance) {
+    						closestCenter = j;
+    						closestCenter2 = j2;
+    						shortestDistance = newDistance;
+    						///System.out.println("Cluster "+closestCenter+" is now the closest with distance "+shortestDistance);
+    					}
+    					newDistance = 0; /// clean the variable for the next cluster.
+    				 }
+    				}	
+    				///In case something went wrong.
+    				if (closestCenter == -1) {
+    					System.out.println("\nWARNING! Something went wrong with assigning to clusters: closestCenter == -1\n");
+    				}
+    				///find all clusters in neighborehood and adjust prototype of them
+    				for (int r = (closestCenter- rad); r < (closestCenter +rad); r++ ){
+    					for (int s = (closestCenter2- rad); s < (closestCenter2 +rad); s++ ){
+    				if((r != closestCenter && s!= closestCenter2) &&(r>=0 )&& (s>=0) && (r<n) && (s<n)){
+    					for (int h = 0; h < dim; h++) {
+    				//clusters[r][s].prototype[h] = (float)((1- learnr) * clusters[r][s].prototype[h]) + (float)(learnr * trainData.get(i)[h]);
+    		         		}
+    				      }
+    					}
+    				}
+    			}
 		//print out progress 
 		System.out.println("progress: " + k + "/" + epochs);
 		}
 		
 		return true;
+
+		
+	}
+	
+	public float[] calculatePrototype(Cluster cluster) {
+		float[] prototype = new float[dim];
+
+		///Get amount of members in the cluster
+		int size = cluster.currentMembers.size();
+
+		///System.out.println("Our cluster contains persons " + cluster.currentMembers);
+
+		///Calculate the mean of all members of the cluster
+		for (int j = 0; j < dim; j++) { /// go through all 200 elements that each vector contains
+
+			float mean = 0; /// initialize to 0 so we can reuse it
+
+			for (int n : cluster.currentMembers) { /// and go through all of the members of the cluster
+				mean = mean + trainData.elementAt(n)[j];
+			}
+			mean = mean / size; /// Calculate the actual mean
+			prototype[j] = mean; /// add it to our prototype
+		}
+		return prototype;
 	}
 	
 	public boolean test()
